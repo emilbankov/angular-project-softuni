75669a43684eb66c665189bad1040d2351af351b
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:42:17 +0200

    Added ipad attach accessory page html

---
+++ angular-project-softuni/src/app/components/ipad/ipad-attach-accessory/ipad-attach-accessory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ipad-attach-accessory',
+  templateUrl: './ipad-attach-accessory.component.html',
+  styleUrls: ['./ipad-attach-accessory.component.css']
+})
+export class iPadAttachAccessoryComponent {
+
+}
