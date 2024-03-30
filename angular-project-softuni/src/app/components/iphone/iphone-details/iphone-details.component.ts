0525ca17b23c81bf878b638fe12e1c2761f6cfe3
emilbankov <emilbankov17d@gmail.com>
2024-03-30 18:51:53 +0200

    Added details page html

---
+++ angular-project-softuni/src/app/components/iphone/iphone-details/iphone-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iphone-details',
+  templateUrl: './iphone-details.component.html',
+  styleUrls: ['./iphone-details.component.css']
+})
+export class iPhoneDetailsComponent {
+
+}
