6f2ecf0e23ed3cc1f0e37131ef707bee04be0257
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:14:37 +0200

    Added imac edit page html

---
+++ angular-project-softuni/src/app/components/imac/imac-edit/imac-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imac-edit',
+  templateUrl: './imac-edit.component.html',
+  styleUrls: ['./imac-edit.component.css']
+})
+export class iMacEditComponent {
+
+}
