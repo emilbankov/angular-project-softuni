43bd586b05e301df809237b1515804082a291109
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:06:52 +0200

    Added imac page html

---
+++ angular-project-softuni/src/app/components/imac/imac-catalog/imac-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imac-catalog',
+  templateUrl: './imac-catalog.component.html',
+  styleUrls: ['./imac-catalog.component.css']
+})
+export class iMacCatalogComponent {
+
+}
