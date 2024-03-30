120a8a53083f46a4a2adc1df1af646ee42c9da09
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:35:59 +0200

    Added ipad details page html

---
+++ angular-project-softuni/src/app/components/ipad/ipad-catalog/ipad-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ipad-catalog',
+  templateUrl: './ipad-catalog.component.html',
+  styleUrls: ['./ipad-catalog.component.css']
+})
+export class iPadCatalogComponent {
+
+}
