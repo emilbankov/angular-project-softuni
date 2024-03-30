2cf1c387847e028db1899209ea60e5cea920387d
emilbankov <emilbankov17d@gmail.com>
2024-03-30 18:52:15 +0200

    Updated routing

---
+++ angular-project-softuni/src/app/components/iphone/iphone-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { iPhoneCatalogComponent } from './iphone-catalog/iphone-catalog.component';
+import { iPhoneEditComponent } from './iphone-edit/iphone-edit.component';
+import { iPhoneDetailsComponent } from './iphone-details/iphone-details.component';
+import { iPhoneAttachAccessoryComponent } from './iphone-attach-accessory/iphone-attach-accessory.component';
+
+const routes: Routes = [
+    {
+        path: 'iphone-catalog',
+        children: [
+            { path: '', pathMatch: 'full', component: iPhoneCatalogComponent },
+            { path: 'iphone-details', component: iPhoneDetailsComponent },
+            { path: 'iphone-attach-accessory', component: iPhoneAttachAccessoryComponent },
+            { path: 'iphone-edit', component: iPhoneEditComponent },
+        ]
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class iPhoneRoutingModule { }
