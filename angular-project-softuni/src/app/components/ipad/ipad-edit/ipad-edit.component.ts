a6b7c8520197d3f97fa9c24505afaa1ab2433a0d
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:40:41 +0200

    Added ipad edit page html

---
+++ angular-project-softuni/src/app/components/ipad/ipad-edit/ipad-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ipad-edit',
+  templateUrl: './ipad-edit.component.html',
+  styleUrls: ['./ipad-edit.component.css']
+})
+export class iPadEditComponent {
+
+}
