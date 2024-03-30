adc77e557c7cd1deb7c7bb9eb1a5de740b027a2d
emilbankov <emilbankov17d@gmail.com>
2024-03-30 18:29:28 +0200

    Added iphone catalog page html

---
+++ angular-project-softuni/src/app/components/iphone/iphone-catalog/iphone-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iphone-catalog',
+  templateUrl: './iphone-catalog.component.html',
+  styleUrls: ['./iphone-catalog.component.css']
+})
+export class iPhoneCatalogComponent {
+
+}
