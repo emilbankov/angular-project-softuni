d3d1e590d5787025b122907972b872964edcb17f
emilbankov <emilbankov17d@gmail.com>
2024-03-30 17:32:18 +0200

    Added add accessories component html

---
+++ angular-project-softuni/src/app/components/add-accessories/add-accessories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-accessories',
+  templateUrl: './add-accessories.component.html',
+  styleUrls: ['./add-accessories.component.css']
+})
+export class AddAccessoriesComponent {
+
+}
