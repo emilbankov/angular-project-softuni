ade02301fc1c19ff6f6295ef66fc08f6539be1fc
emilbankov <emilbankov17d@gmail.com>
2024-03-30 18:51:07 +0200

    Created edit page with html

---
+++ angular-project-softuni/src/app/components/iphone/iphone-edit/iphone-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-iphone-edit',
+  templateUrl: './iphone-edit.component.html',
+  styleUrls: ['./iphone-edit.component.css']
+})
+export class iPhoneEditComponent {
+
+}
