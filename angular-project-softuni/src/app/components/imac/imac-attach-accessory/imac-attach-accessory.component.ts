e2854c8ba45f97af7cefbb0fa62458f44e54a332
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:17:47 +0200

    Added imac attach accessory page html

---
+++ angular-project-softuni/src/app/components/imac/imac-attach-accessory/imac-attach-accessory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imac-attach-accessory',
+  templateUrl: './imac-attach-accessory.component.html',
+  styleUrls: ['./imac-attach-accessory.component.css']
+})
+export class iMacAttachAccessoryComponent {
+
+}
