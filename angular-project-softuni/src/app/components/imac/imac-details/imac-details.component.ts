d76d7aff8333a5575c91ffc1e1ab2c29ae5525a2
emilbankov <emilbankov17d@gmail.com>
2024-03-30 19:12:36 +0200

    Added imac details page html

---
+++ angular-project-softuni/src/app/components/imac/imac-details/imac-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imac-details',
+  templateUrl: './imac-details.component.html',
+  styleUrls: ['./imac-details.component.css']
+})
+export class iMacDetailsComponent {
+
+}
