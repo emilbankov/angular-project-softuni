caf58d12927e9b8549f478ed60f894d563d69805
emilbankov <emilbankov17d@gmail.com>
2024-03-31 15:28:02 +0300

    Used api service in imac catalog

--- client/src/app/components/imac/imac-catalog/imac-catalog.component.ts
+++ client/src/app/components/imac/imac-catalog/imac-catalog.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-imac-catalog',
   templateUrl: './imac-catalog.component.html',
   styleUrls: ['./imac-catalog.component.css']
 })
-export class iMacCatalogComponent {
+export class iMacCatalogComponent implements OnInit {
 
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getImacs().subscribe((imacs) => {
+      console.log(imacs);
+    })
+  }
+
+  // ngOnInit(): void {
+  //   this.api.getImacs().subscribe((imacs) => {
+  //     console.log(imacs);
+  //   })
+  // }
 }
