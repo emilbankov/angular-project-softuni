e63cc2c1e4060eca131a482ac690fcb2f6c2d634
emilbankov <emilbankov17d@gmail.com>
2024-03-31 15:27:21 +0300

    Added imac interface to types folder

---
+++ client/src/app/types/imac.ts
@@ -0,0 +1,13 @@
+export interface iMac {
+    _ownerId: string,
+    name: string,
+    imageUrl: string,
+    cpu: string,
+    gpu: string,
+    description: string,
+    display: string,
+    storage: string,
+    price: string,
+    _createdOn: number,
+    _id: string
+}
