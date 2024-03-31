b8f57093b03da96cb123f2bd2043307f456331a5
emilbankov <emilbankov17d@gmail.com>
2024-03-31 15:26:41 +0300

    Created types folder and added iphone interface

---
+++ client/src/app/types/iphone.ts
@@ -0,0 +1,13 @@
+export interface iPhone {
+    _ownerId: string,
+    name: string,
+    imageUrl: string,
+    color: string,
+    batteryLife: string,
+    description: string,
+    display: string,
+    storage: string,
+    price: string,
+    _createdOn: number,
+    _id: string
+}
