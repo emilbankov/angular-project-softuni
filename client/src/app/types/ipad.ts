33e38697c614479a78ba0d8559f41e71253e5e74
emilbankov <emilbankov17d@gmail.com>
2024-03-31 15:27:10 +0300

    Added ipad interface to types folder

---
+++ client/src/app/types/ipad.ts
@@ -0,0 +1,13 @@
+export interface iPad {
+    _ownerId: string,
+    name: string,
+    imageUrl: string,
+    color: string,
+    batteryLife: string,
+    description: string,
+    display: string,
+    storage: string,
+    price: string,
+    _createdOn: number,
+    _id: string
+}
