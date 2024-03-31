cd9b2a0121fc3f5265d301d3b61d622411e1828c
emilbankov <emilbankov17d@gmail.com>
2024-03-31 16:40:36 +0300

    Dynamically loading ipad catalog products

--- client/src/app/components/ipad/ipad-catalog/ipad-catalog.component.ts
+++ client/src/app/components/ipad/ipad-catalog/ipad-catalog.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { iPad } from 'src/app/types/ipad';
 
 @Component({
   selector: 'app-ipad-catalog',
   templateUrl: './ipad-catalog.component.html',
   styleUrls: ['./ipad-catalog.component.css']
 })
-export class iPadCatalogComponent {
+export class iPadCatalogComponent implements OnInit {
+  ipads: iPad[] | null = [];
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getIpads().subscribe((ipads) => {
+      this.ipads = ipads;
+    })
+  }
 
 }
