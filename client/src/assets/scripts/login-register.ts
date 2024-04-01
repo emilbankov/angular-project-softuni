46ecd1d6e3bc49d71c98e8b4b1862b3a5fcb2a48
emilbankov <emilbankov17d@gmail.com>
2024-04-01 14:16:34 +0300

    Separated login-register toggle functionality in login-register ts file

---
+++ client/src/assets/scripts/login-register.ts
@@ -0,0 +1,17 @@
+import { Renderer2 } from '@angular/core';
+
+export function loginRegister(renderer: Renderer2): void {
+  const container = document.getElementById('container');
+  const registerBtn = document.getElementById('register');
+  const loginBtn = document.getElementById('login');
+
+  if (container && registerBtn && loginBtn) {
+    registerBtn.addEventListener('click', () => {
+      renderer.addClass(container, "active");
+    });
+
+    loginBtn.addEventListener('click', () => {
+      renderer.removeClass(container, "active");
+    });
+  } 
+}
