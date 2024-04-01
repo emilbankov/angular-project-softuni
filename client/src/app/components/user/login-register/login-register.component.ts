6c9e0e1eeab50a92af09dbd1656684cf68c108cd
emilbankov <emilbankov17d@gmail.com>
2024-04-01 15:51:42 +0300

    Working error validation for register

--- client/src/app/components/user/login-register/login-register.component.ts
+++ client/src/app/components/user/login-register/login-register.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, AfterViewInit, Renderer2 } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { loginRegisterToggle } from 'src/assets/scripts/login-register';
+import { emailValidator } from '../../utils/email-validator';
+import { matchPasswordsValidator } from '../../utils/match-passwords';
 
 @Component({
   selector: 'app-login-register',
@@ -6,28 +10,36 @@ import { Component, OnInit, AfterViewInit, Renderer2 } from '@angular/core';
   styleUrls: ['./login-register.component.css']
 })
 export class LoginRegisterComponent implements AfterViewInit {
+  form = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    passGroup: this.fb.group({
+      password: ['', [Validators.required]],
+      rePassword: ['', [Validators.required]]
+    }, {
+      validators: [matchPasswordsValidator('password', 'rePassword')]
+    })
+  })
 
-  constructor(private renderer: Renderer2) { }
+  constructor(private renderer: Renderer2, private fb: FormBuilder) {}
+
+  register() {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+
+  }
 
   ngAfterViewInit(): void {
-    this.executeScript();
+    loginRegisterToggle(this.renderer);
   }
 
-  private executeScript(): void {
-    const container = document.getElementById('container');
-    const registerBtn = document.getElementById('register');
-    const loginBtn = document.getElementById('login');
-
-    if (container && registerBtn && loginBtn) {
-      registerBtn.addEventListener('click', () => {
-        container.classList.add("active");
-      });
-
-      loginBtn.addEventListener('click', () => {
-        container.classList.remove("active");
-      });
-    } else {
-      console.error("One or more elements not found.");
-    }
-  }
+  get email() { return this.form.get('email'); }
+  get isEmailRequired() { return this.form.get('email')?.errors?.['required']; }
+  get isEmailValid() { return this.form.get('email')?.errors?.['emailValidator']; }
+  get password() { return this.form.get('passGroup')?.get('password'); }
+  get isPasswordRequired() { return this.form.get('passGroup')?.get('password')?.errors?.['required']; }
+  get rePassword() { return this.form.get('passGroup')?.get('rePassword'); }
+  get isrePasswordRequired() { return this.form.get('passGroup')?.get('password')?.errors?.['required']; }
+  get areMatching() { return this.form.get('passGroup')?.errors?.['matchPasswordsValidator']; }
 }
