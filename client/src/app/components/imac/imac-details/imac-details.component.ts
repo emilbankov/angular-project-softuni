45a7bed76d363dfd14512684be36b81a63024432
emilbankov <emilbankov17d@gmail.com>
2024-04-01 12:03:12 +0300

    Added working details functionality for imac

--- client/src/app/components/imac/imac-details/imac-details.component.ts
+++ client/src/app/components/imac/imac-details/imac-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { iMac } from 'src/app/types/imac';
 
 @Component({
   selector: 'app-imac-details',
   templateUrl: './imac-details.component.html',
   styleUrls: ['./imac-details.component.css']
 })
-export class iMacDetailsComponent {
+export class iMacDetailsComponent implements OnInit {
+  imac = {} as iMac;
+  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) { }
 
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      const id = data['imacId'];
+      this.apiService.getImac(id).subscribe((imac) => {
+        this.imac = imac;
+      })
+    })
+  }
 }
