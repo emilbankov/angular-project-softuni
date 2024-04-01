9779ad0618d071f60ca46a5315065c2d59a246fb
emilbankov <emilbankov17d@gmail.com>
2024-04-01 18:40:38 +0300

    Displaying errors for add accessory form inputs

--- client/src/app/components/add-accessories/add-accessories.component.ts
+++ client/src/app/components/add-accessories/add-accessories.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-accessories',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-accessories.component.css']
 })
 export class AddAccessoriesComponent {
-
+  addIphone(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+  }
 }
