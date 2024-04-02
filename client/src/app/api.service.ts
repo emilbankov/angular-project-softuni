83007024c8c3f74efc22cf2bec1caba6d41889f9
emilbankov <emilbankov17d@gmail.com>
2024-04-02 19:31:20 +0300

    Changed api service

--- client/src/app/api.service.ts
+++ client/src/app/api.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { iMac } from './types/imac';
-import { iPad } from './types/ipad';
+import iPad from './types/ipad';
 import { iPhone } from './types/iphone';
+import { get, post } from './components/utils/request';
 
 @Injectable({
   providedIn: 'root'
@@ -10,44 +11,44 @@ import { iPhone } from './types/iphone';
 export class ApiService {
   baseUrl = 'http://localhost:3030/data';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   // iPhone Services
   getAllIphones() {
-    return this.http.get<iPhone[]>(`${this.baseUrl}/iphones`);
+    return get(`${this.baseUrl}/iphones`) as Promise<iPhone[]>;
   }
 
   getIphone(id: string) {
-    return this.http.get<iPhone>(`${this.baseUrl}/iphones/${id}`);
+    return get(`${this.baseUrl}/iphones/${id}`) as Promise<iPhone>;
   }
 
   addIphone(name: string, imageUrl: string, color: string, batteryLife: string, description: string, display: string, storage: string, price: string) {
-    return this.http.post<iPhone>(`${this.baseUrl}/iphones`, { name, imageUrl, color, batteryLife, description, display, storage, price })
+    return post(`${this.baseUrl}/iphones`, { name, imageUrl, color, batteryLife, description, display, storage, price }) as Promise<iPhone>;
   }
 
   // iPad Services
   getAllIpads() {
-    return this.http.get<iPad[]>(`${this.baseUrl}/ipads`);
+    return get(`${this.baseUrl}/ipads`) as Promise<iPad[]>;
   }
 
   getIpad(id: string) {
-    return this.http.get<iPad>(`${this.baseUrl}/ipads/${id}`);
+    return get(`${this.baseUrl}/ipads/${id}`) as Promise<iPad>;
   }
 
   addIpad(name: string, imageUrl: string, color: string, batteryLife: string, description: string, display: string, storage: string, price: string) {
-    return this.http.post<iPad>(`${this.baseUrl}/ipads`, { name, imageUrl, color, batteryLife, description, display, storage, price })
+    return post(`${this.baseUrl}/ipads`, { name, imageUrl, color, batteryLife, description, display, storage, price }) as Promise<iPad>;
   }
 
   // iMac Services
   getAllImacs() {
-    return this.http.get<iMac[]>(`${this.baseUrl}/imacs`);
+    return get(`${this.baseUrl}/imacs`) as Promise<iMac[]>;
   }
 
   getImac(id: string) {
-    return this.http.get<iMac>(`${this.baseUrl}/imacs/${id}`);
+    return get(`${this.baseUrl}/imacs/${id}`) as Promise<iMac>;
   }
 
   addImac(name: string, imageUrl: string, cpu: string, gpu: string, description: string, display: string, storage: string, price: string) {
-    return this.http.post<iMac>(`${this.baseUrl}/imacs`, { name, imageUrl, cpu, gpu, description, display, storage, price })
+    return post(`${this.baseUrl}/imacs`, { name, imageUrl, cpu, gpu, description, display, storage, price }) as Promise<iMac>;
   }
 }
