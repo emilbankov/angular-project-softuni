69f399da5a7a3776d8b3cc20105ead3e39d8f0b9
emilbankov <emilbankov17d@gmail.com>
2024-04-03 01:05:31 +0300

    Working imac edit functionality

--- client/src/app/components/imac/imac-edit/imac-edit.component.ts
+++ client/src/app/components/imac/imac-edit/imac-edit.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-imac-edit',
   templateUrl: './imac-edit.component.html',
   styleUrls: ['./imac-edit.component.css']
 })
-export class iMacEditComponent {
+export class iMacEditComponent implements OnInit {
+  @ViewChild('editImacForm') editImacForm!: NgForm;
 
+  constructor(private apiService: ApiService, private router: Router, private activatedRoute: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      const id = data['imacId'];
+      this.apiService.getImac(id).subscribe((imac) => {
+        const { name, imageUrl, cpu, gpu, description, display, storage, price } = imac;
+        
+        if (this.editImacForm) {
+          this.editImacForm.setValue({ name, imageUrl, cpu, gpu, description, display, storage, price })
+        }
+      })
+    })
+  }
+
+  editImac(editImacForm: NgForm) {
+    if (editImacForm.invalid) {
+      return;
+    }
+
+    this.activatedRoute.params.subscribe((data) => {
+      const id = data['imacId']
+      const { name, imageUrl, cpu, gpu, description, display, storage, price } = editImacForm.value;
+      this.apiService.editImac(id, name, imageUrl, cpu, gpu, description, display, storage, price).subscribe(() => {
+        this.router.navigate([`/imac-catalog/imac-details/${id}`])
+      })
+    })
+
+  }
 }
