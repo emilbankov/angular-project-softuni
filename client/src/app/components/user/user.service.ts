66387cfdad7087831aa6ab681d8fdd7dba6a7614
emilbankov <emilbankov17d@gmail.com>
2024-04-02 16:26:50 +0300

    Updated user service to have working login, register and logout functionality

--- client/src/app/components/user/user.service.ts
+++ client/src/app/components/user/user.service.ts
@@ -1,16 +1,41 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { UserAuth } from 'src/app/types/user';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnDestroy {
+  private user$$ = new BehaviorSubject<UserAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
 
-  constructor(private http: HttpClient) { }
+  user: UserAuth | undefined;
+  userSubscription: Subscription;
 
-  login(email: string, password: string) {
-    return this.http.post('/users/login', { email, password });
+  isLoggedIn() {
+    return !!localStorage.getItem('accessToken');
   }
 
-  
+  constructor(private http: HttpClient) {
+    this.userSubscription = this.user$.subscribe(user => {
+      this.user = user
+    })
+  }
+ 
+  login(email: string, password: string) {
+    return this.http.post<UserAuth>('http://localhost:3030/users/login', { email, password }).pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  register(email: string, password: string) {
+    return this.http.post<UserAuth>('http://localhost:3030/users/register', { email, password }).pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  logout() {
+    return this.http.post<UserAuth>('http://localhost:3030/users/logout', {}).pipe(tap(() => this.user$$.next(undefined)));
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
 }
