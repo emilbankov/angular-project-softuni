905863d7a876452d26b2a56e83e3dab2644d8261
emilbankov <emilbankov17d@gmail.com>
2024-04-03 17:21:06 +0300

    Working delete functionality for imacs

--- client/src/app/components/imac/imac-details/imac-details.component.ts
+++ client/src/app/components/imac/imac-details/imac-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { iMac } from 'src/app/types/imac';
 
@@ -10,7 +10,7 @@ import { iMac } from 'src/app/types/imac';
 })
 export class iMacDetailsComponent implements OnInit {
   imac = {} as iMac;
-  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute) { }
+  constructor(private apiService: ApiService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe((data) => {
@@ -18,6 +18,18 @@ export class iMacDetailsComponent implements OnInit {
       this.apiService.getImac(id).subscribe((imac) => {
         this.imac = imac;
       })
+    })
+  }
+
+  deleteImac() {
+    this.activatedRoute.params.subscribe((data) => {
+      const id = data['imacId'];
+      const hasConfirmed = confirm(`Are you sure you want to delete ${this.imac.name}`);
+      if (hasConfirmed) {
+        this.apiService.deleteImac(id).subscribe(() => {
+          this.router.navigate(['/imac-catalog'])
+        })
+      }
     })
   }
 }
